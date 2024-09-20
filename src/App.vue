4d4cf0e050084f3d5ec6e0c6c6c28bd09649dac9
Caeilanlightwood77 <sinahanbilly@outlook.com>
2024-09-20 21:53:54 +0800

    Updated task form with priority levels and reminder logic

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-container fluid class="pa-0">
       <v-row no-gutters>
         <!-- Sidebar with Task Lists -->
-        <v-col cols="3" class="sidebar">
+        <v-col cols="12" md="3" class="sidebar">
           <v-toolbar flat color="primary" dark class="mb-2">
             <v-toolbar-title>My Lists</v-toolbar-title>
           </v-toolbar>
@@ -25,12 +25,11 @@
                 </v-list-item-title>
               </v-list-item-content>
               <v-list-item-action>
-                <v-icon
-                  @click.stop="deleteList(index)"
-                  class="delete-icon"
-                >
+                <v-icon @click.stop="deleteList(index)" class="delete-icon">
                   mdi-delete
                 </v-icon>
+                <v-icon @click.stop="editList(index)">mdi-pencil</v-icon>
+                <!-- Edit List Icon -->
               </v-list-item-action>
             </v-list-item>
 
@@ -41,12 +40,13 @@
         </v-col>
 
         <!-- Main Content Area with Tasks -->
-        <v-col cols="9" class="content-area">
+        <v-col cols="12" md="9" class="content-area">
           <v-toolbar flat color="primary" dark class="mb-2">
-            <v-toolbar-title>{{ selectedList.name || 'Select a List' }}</v-toolbar-title>
+            <v-toolbar-title>{{
+              selectedList.name || "Select a List"
+            }}</v-toolbar-title>
           </v-toolbar>
 
-          <!-- Conditionally display the task input and list if a list is selected -->
           <v-container v-if="selectedList.name">
             <v-row>
               <!-- Task Input Form -->
@@ -76,7 +76,9 @@
                     </v-list-item-action>
 
                     <v-list-item-content>
-                      <v-list-item-title :class="{ 'completed-task': task.completed }">
+                      <v-list-item-title
+                        :class="{ 'completed-task': task.completed }"
+                      >
                         {{ task.title }}
                       </v-list-item-title>
                     </v-list-item-content>
@@ -88,6 +90,8 @@
                       >
                         mdi-delete
                       </v-icon>
+                      <v-icon @click.stop="editTask(index)">mdi-pencil</v-icon>
+                      <!-- Edit Task Icon -->
                     </v-list-item-action>
                   </v-list-item>
                 </v-list>
@@ -95,7 +99,6 @@
             </v-row>
           </v-container>
 
-          <!-- Display a message prompting the user to select a list if none is selected -->
           <v-container v-else>
             <v-row>
               <v-col cols="12" class="text-center">
@@ -110,7 +113,9 @@
     <!-- New List Dialog -->
     <v-dialog v-model="newListDialog" max-width="500px">
       <v-card>
-        <v-card-title>Add New List</v-card-title>
+        <v-card-title>{{
+          editMode ? "Edit List" : "Add New List"
+        }}</v-card-title>
         <v-card-text>
           <v-text-field
             v-model="newListName"
@@ -127,7 +132,44 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" @click="createNewList">Create</v-btn>
+          <v-btn
+            color="primary"
+            @click="editMode ? updateList() : createNewList()"
+          >
+            {{ editMode ? "Update" : "Create" }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- Task Edit Dialog -->
+    <v-dialog v-model="taskEditDialog" max-width="500px">
+      <v-card>
+        <v-card-title>Edit Task</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="taskToEdit.title"
+            label="Task Title"
+            outlined
+            dense
+          ></v-text-field>
+          <v-text-field
+            v-model="taskToEdit.date"
+            label="Task Date"
+            outlined
+            dense
+          ></v-text-field>
+          <v-select
+            v-model="taskToEdit.repeat"
+            :items="['Never', 'Daily', 'Weekly', 'Monthly']"
+            label="Repeat"
+            outlined
+            dense
+          ></v-select>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="updateTask">Save Task</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -140,10 +182,15 @@ export default {
     return {
       taskLists: [],
       selectedList: {},
-      newTaskTitle: '',
+      newTaskTitle: "",
       newListDialog: false,
-      newListName: '',
-      newListColor: '#FF5722',
+      newListName: "",
+      newListColor: "#FF5722",
+      editMode: false,
+      selectedListIndex: null,
+      taskEditDialog: false,
+      editingTaskIndex: null,
+      taskToEdit: {},
     };
   },
   methods: {
@@ -152,31 +199,53 @@ export default {
     },
     addTask() {
       if (this.newTaskTitle) {
-        this.selectedList.tasks.push({
+        const task = {
           title: this.newTaskTitle,
           completed: false,
-        });
-        this.newTaskTitle = '';
+          date: this.taskDate,
+          repeat: this.repeat,
+          priority: this.priority,
+          reminderTime: this.reminderTime, // New property for reminder time
+        };
+        this.selectedList.tasks.push(task);
+
+        // Calculate the reminder time in milliseconds
+        const reminderTimeMs = task.reminderTime * 60 * 1000; // Convert minutes to milliseconds
+        const dueDate = new Date(task.date);
+        const currentDate = new Date();
+
+        // Check if the due date is in the future
+        if (dueDate > currentDate) {
+          const timeUntilReminder =
+            dueDate.getTime() - currentDate.getTime() - reminderTimeMs;
+
+          // Set a timeout for the reminder
+          if (timeUntilReminder > 0) {
+            setTimeout(() => {
+              alert(`Reminder: Task "${task.title}" is due soon!`);
+            }, timeUntilReminder);
+          }
+        }
+
+        // Reset task fields
+        this.newTaskTitle = "";
+        this.reminderTime = null;
       }
-    },
-    toggleTaskCompletion(index) {
-      this.selectedList.tasks[index].completed =
-        !this.selectedList.tasks[index].completed;
     },
     deleteTask(index) {
       this.selectedList.tasks.splice(index, 1);
     },
     deleteList(index) {
-      // Remove the list
       const removedList = this.taskLists.splice(index, 1)[0];
-      
-      // If the deleted list was selected, clear the selected list and tasks
       if (this.selectedList === removedList) {
         this.selectedList = {};
       }
     },
     openNewListDialog() {
       this.newListDialog = true;
+      this.editMode = false;
+      this.newListName = "";
+      this.newListColor = "#FF5722";
     },
     createNewList() {
       if (this.newListName) {
@@ -185,12 +254,35 @@ export default {
           color: this.newListColor,
           tasks: [],
         });
-        this.newListName = '';
-        this.newListColor = '#FF5722';
         this.newListDialog = false;
-
-        // Automatically select the newly created list
         this.selectedList = this.taskLists[this.taskLists.length - 1];
+      }
+    },
+    editList(index) {
+      this.selectedListIndex = index;
+      this.newListName = this.taskLists[index].name;
+      this.newListColor = this.taskLists[index].color;
+      this.newListDialog = true;
+      this.editMode = true;
+    },
+    updateList() {
+      if (this.selectedListIndex !== null) {
+        this.taskLists[this.selectedListIndex].name = this.newListName;
+        this.taskLists[this.selectedListIndex].color = this.newListColor;
+        this.newListDialog = false;
+        this.selectedListIndex = null;
+      }
+    },
+    editTask(taskIndex) {
+      this.editingTaskIndex = taskIndex;
+      this.taskToEdit = { ...this.selectedList.tasks[taskIndex] };
+      this.taskEditDialog = true;
+    },
+    updateTask() {
+      if (this.editingTaskIndex !== null) {
+        this.selectedList.tasks[this.editingTaskIndex] = this.taskToEdit;
+        this.taskEditDialog = false;
+        this.editingTaskIndex = null;
       }
     },
   },
@@ -229,7 +321,8 @@ export default {
 }
 
 .v-toolbar {
-  background-color: #0078d7 !important; /* Microsoft To Do-like blue */
+  background-color: #0078d7 !important;
+  /* Microsoft To Do-like blue */
 }
 
 .v-btn {
@@ -256,6 +349,7 @@ export default {
 
 .delete-icon {
   cursor: pointer;
-  color: red; /* Red color for delete icon */
+  color: red;
+  /* Red color for delete icon */
 }
 </style>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,52 +1,45 @@
 <template>
-    <v-container>
-      <v-card class="pa-4 mb-4">
-        <ListForm @add-list="addList"></ListForm>
-      </v-card>
-  
-      <v-row>
-        <v-col
-          v-for="list in lists"
-          :key="list.name"
-          cols="12" sm="6" md="4"
-        >
-          <v-card :style="{ backgroundColor: list.color }" class="pa-4">
-            <v-card-title>{{ list.name }}</v-card-title>
-            <v-divider></v-divider>
-            <v-list dense>
-              <v-list-item
-                v-for="task in list.tasks"
-                :key="task.title"
-              >
-                <v-list-item-content>
-                  <v-list-item-title>{{ task.title }}</v-list-item-title>
-                  <v-list-item-subtitle>
-                    Schedule: {{ task.date }} | Repeat: {{ task.repeat }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  import ListForm from '@/components/ListForm.vue';
-  
-  export default {
-    components: { ListForm },
-    data() {
-      return {
-        lists: [],
-      };
+  <v-container>
+    <v-card class="pa-4 mb-4">
+      <ListForm @add-list="addList"></ListForm>
+    </v-card>
+
+    <v-row>
+      <v-col v-for="list in lists" :key="list.name" cols="12" sm="6" md="4">
+        <v-card :style="{ backgroundColor: list.color }" class="pa-4">
+          <v-card-title>{{ list.name }}</v-card-title>
+          <v-divider></v-divider>
+          <v-list dense>
+            <v-list-item v-for="task in list.tasks" :key="task.title">
+              <v-list-item-content>
+                <v-list-item-title>{{ task.title }}</v-list-item-title>
+                <v-list-item-subtitle>
+                  Due: {{ task.date }} | Repeat: {{ task.repeat }} | Priority:
+                  {{ task.priority }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ListForm from "@/components/ListForm.vue";
+
+export default {
+  components: { ListForm },
+  data() {
+    return {
+      lists: [],
+    };
+  },
+  methods: {
+    addList(list) {
+      this.lists.push(list);
     },
-    methods: {
-      addList(list) {
-        this.lists.push(list);
-      },
-    },
-  };
-  </script>
-  
+  },
+};
+</script>
